90743cbdb50a1eac71c469d345510502ad236036
Majorfelps <michael.as.cruz@gmail.com>
2025-01-15 00:10:20 -0300

    Hero and components initial

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
     return (
@@ -10,7 +11,8 @@ const Hero = () => {
             className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
             >
                 <source src='/blackhole.webm' type='video/webm'/>
-            </video>            
+            </video>
+            <HeroContent />            
         </div>
     )
 }
