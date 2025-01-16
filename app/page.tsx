f9fbffd9c82059ec533dd55eabc1c009438c1760
majorfelps <michael.as>
2025-01-16 16:31:00 -0300

    final build portfolio space

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Encryption from "@/components/main/Encryption";
 import Hero from "../components/main/Hero"
-
+import Skill from "@/components/main/Skill";
+import Projects from "@/components/main/Projects";
 
 export default function Home() {
   return (
     <main className="h-full w-full ">
       <div className="flex flex-col gap-20">
         <Hero />
+        <Skill />
+        <Encryption />
+        <Projects />
       </div>
     </main>
   );
--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Image from 'next/image'
+import { Socials } from '@/constants'
 
 const Navbar = () => {
   return (
     <div className='w-full h-[65px] fixed top-0 shadow-lg
-     shadow-[#2a0e61]/50 bg-[#03001417] backdrop-blur-md z-50 px-18'>
-        Navbar
+       shadow-[#2a0e61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
+        <div className="w-full h-full flex flex-row item-center justify-between m-auto px-[10px]">
+          <a href="#about-me" className='h-auto w-auto flex flex-row items-center'>
+            <Image
+              src="/NavLogo.png"
+              alt="logo"
+              width={50}
+              height={50}
+              className='cursor-pointer hover:animate-slowspin'            
+            />
+            <span className='font-bold ml-[10px] hiddne md:block text-gray-300'>
+              Goliath Corp.
+            </span>
+          </a>
+          <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
+            <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] 
+              mr-[15px] px-[20px] py-[15px] rounded-full text-gray-200'>
+          <a href="#about-me" className='cursor-pointer'>About me</a>
+          <a href="#skills" className='cursor-pointer'>Skill</a>
+          <a href="#projects" className='cursor-pointer'>Projects</a>
+            </div>
+          </div>
+          <div className="flex flex-row gap-5">
+            {Socials.map((social) => (
+              <Image
+                src={social.src}
+                alt={social.name}
+                key={social.name}
+                width={24}
+                height={24}
+              />
+            ))}
+          </div>
+        </div>           
     </div>
   )
 }
